941f12361718604d0c7dbab67809644ed6364149
jojo <jojojablon@gmail.com>
2023-08-16 10:08:55 -0300

    funciones en Quality

--- Quality.hs
+++ Quality.hs
@@ -6,3 +6,11 @@ data Quality = Qua String Int Float deriving (Eq, Show)
 newQ :: String -> Int -> Float -> Quality
 capacityQ :: Quality -> Int -- cuantos túneles puede tolerar esta conexión
 delayQ :: Quality -> Float  -- la demora por unidad de distancia que sucede en las conexiones de este canal
+
+newQ name tol delay = Qua name tol delay
+capacityQ (Qua name tol delay) = tol
+delayQ (Qua name tol delay) = delay
+
+qual1 = Qua "pepe" 10 3
+capQ1 = capacityQ qual1
+delQ1 = delayQ qual1
