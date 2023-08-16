8c76c55d1d27c2147cb7c85748084bc64c5dd3f4
jojo <jojojablon@gmail.com>
2023-08-16 11:31:54 -0300

    funciones City y Link

--- City.hs
+++ City.hs
@@ -16,5 +16,5 @@ distanceC (Cit name1 point1) (Cit name2 point2) = difP point1 point2
 p1 = newP 1 1
 ciudad1 = Cit "hola" p1
 p2 = newP 2 2
-ciudad2 = Cit "igna" p2
+ciudad2 = Cit "chau" p2
 disC1C2 = distanceC ciudad1 ciudad2
--- Link.hs
+++ Link.hs
@@ -1,6 +1,10 @@
 module Link ( Link, newL, linksL, connectsL, capacityL, delayL )
    where
 
+import Point
+import City
+import Quality
+
 data Link = Lin City City Quality deriving (Eq, Show)
 
 newL :: City -> City -> Quality -> Link -- genera un link entre dos ciudades distintas
@@ -8,3 +12,29 @@ connectsL :: City -> Link -> Bool   -- indica si esta ciudad es parte de este li
 linksL :: City -> City -> Link -> Bool -- indica si estas dos ciudades distintas estan conectadas mediante este link
 capacityL :: Link -> Int
 delayL :: Link -> Float     -- la demora que sufre una conexion en este canal
+
+newL city1 city2 qual = Lin city1 city2 qual
+connectsL cityIn (Lin city1 city2 qual) | cityIn == city1 || cityIn == city2 = True
+                                        | otherwise = False
+linksL city1 city2 (Lin city3 city4 qual) | (city1 == city3 && city2 == city4) || (city1 == city4 && city2 == city3) = True
+                                          | otherwise = False
+
+capacityL = undefined
+delayL = undefined
+
+
+p1 = newP 1 1
+ciudad1 = newC "hola" p1
+p2 = newP 2 2
+ciudad2 = newC "chau" p2
+qual1 = newQ "pepe" 10 3
+link1 = Lin ciudad1 ciudad2 qual1
+
+p3 = newP 4 4
+ciudad3 = newC "juan" p3
+connect1 = connectsL ciudad1 link1
+connect2 = connectsL ciudad3 link1
+
+link2 = linksL ciudad1 ciudad2 link1
+link3 = linksL ciudad2 ciudad1 link1
+link4 = linksL ciudad3 ciudad2 link1
