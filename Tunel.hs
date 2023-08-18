023ad2efd5af5d272bdba9f29014ff981acdaeb5
jojo <jojojablon@gmail.com>
2023-08-18 13:01:39 -0300

    chequeo de que las ciudades esten en el link

--- Tunel.hs
+++ Tunel.hs
@@ -14,7 +14,16 @@ usesT :: Link -> Tunel -> Bool  -- indica si este tunel atraviesa ese link
 delayT :: Tunel -> Float -- la demora que sufre una conexion en este tunel
 
 newT links = Tun links
-connectsT = undefined
+
+connectsT city1 city2 tun | city1 == city2 = error "Same city"
+                          | (cityInTunel city1 city2 tun) == False = error "At least one city not in tunel"
+                          | otherwise = True
+                        --   | otherwise foldr(\each fold -> if )
+
+
+cityInTunel :: City -> City -> Tunel -> Bool
+cityInTunel city1 city2 (Tun (tun : tunvs)) = connectsL city1 tun && connectsL city2 tun
+
 usesT = undefined
 delayT = undefined
 
@@ -29,3 +38,14 @@ ciudad3 = newC "jamaica" p3
 qual2 = newQ "cuba" 4 2
 link2 = newL ciudad1 ciudad3 qual2
 tunel1 = newT [link1, link2]
+con1 = connectsT ciudad1 ciudad2 tunel1
+con2 = connectsT ciudad1 ciudad3 tunel1
+
+{-
+
+combinations :: [a] -> [[a]]
+combinations xs = foldr combine [[]] xs
+    where
+        combine x acc = [ y | y <- acc, y' <- [x]:y ]
+
+-}
