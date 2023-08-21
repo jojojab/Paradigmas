df26304b7c790c404147abb5300b1d80865accf8
jojo <jojojablon@gmail.com>
2023-08-21 19:37:59 -0300

    funciones tunel

--- Tunel.hs
+++ Tunel.hs
@@ -16,16 +16,24 @@ delayT :: Tunel -> Float -- la demora que sufre una conexion en este tunel
 newT links = Tun links
 
 connectsT city1 city2 tun | city1 == city2 = error "Same city"
-                          | (cityInTunel city1 city2 tun) == False = error "At least one city not in tunel"
-                          | otherwise = True
-                        --   | otherwise foldr(\each fold -> if )
+                          | firstCity city1 tun && lastCity city2 tun = True
+                          | firstCity city2 tun && lastCity city1 tun = True
+                          | otherwise = False
 
 
-cityInTunel :: City -> City -> Tunel -> Bool
-cityInTunel city1 city2 (Tun (tun : tunvs)) = connectsL city1 tun && connectsL city2 tun
+-- cityInTunel :: City -> City -> Tunel -> Bool
+-- cityInTunel city1 city2 (Tun (tun : tunvs)) = connectsL city1 tun && connectsL city2 tun
 
-usesT = undefined
-delayT = undefined
+firstCity :: City -> Tunel -> Bool
+firstCity city (Tun links) = connectsL city (head links) && not (connectsL city (head (tail links)))
+
+lastCity :: City -> Tunel -> Bool
+lastCity city (Tun links) = connectsL city (last links) && not (connectsL city (head (init links)))
+
+
+usesT linkTarget (Tun links) = foldr (\each fold -> if each == linkTarget then True else False) False links
+
+delayT (Tun links) = foldr (\each fold -> fold + delayL each) 0.0 links 
 
 p1 = newP 1 1
 ciudad1 = newC "hola" p1
@@ -36,16 +44,19 @@ link1 = newL ciudad1 ciudad2 qual1
 p3 = newP 4 8
 ciudad3 = newC "jamaica" p3
 qual2 = newQ "cuba" 4 2
-link2 = newL ciudad1 ciudad3 qual2
-tunel1 = newT [link1, link2]
+link2 = newL ciudad2 ciudad3 qual2
+qual3 = newQ "panama" 5 10
+link3 = newL ciudad3 ciudad1 qual3
+tunel1 = Tun [link1, link2]
+tunel2 = Tun [link2, link3]
+
 con1 = connectsT ciudad1 ciudad2 tunel1
 con2 = connectsT ciudad1 ciudad3 tunel1
+con3 = connectsT ciudad3 ciudad1 tunel1
 
-{-
+uses1 = usesT link1 tunel1
+uses2 = usesT link3 tunel1
+uses3 = usesT link3 tunel2
 
-combinations :: [a] -> [[a]]
-combinations xs = foldr combine [[]] xs
-    where
-        combine x acc = [ y | y <- acc, y' <- [x]:y ]
-
--}
+delay1 = delayT tunel1
+delay2 = delayT tunel2
