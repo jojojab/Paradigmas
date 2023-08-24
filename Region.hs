1cb65208f3896eaee49939c273efddbd9de9f9b1
jojo <jojojablon@gmail.com>
2023-08-24 14:16:34 -0300

    Mejor tunelR y otras 2 funciones

--- Region.hs
+++ Region.hs
@@ -1,5 +1,3 @@
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# HLINT ignore "Redundant if" #-}
 module Region ( Region, newR, foundR, linkR, tunelR, connectedR, linkedR, delayR, availableCapacityForR)
    where
 
@@ -15,28 +13,19 @@ newR :: Region
 newR = Reg [] [] []
 
 foundR :: Region -> City -> Region -- agrega una nueva ciudad a la región
-foundR (Reg citys links tunels) newCity = Reg (citys ++ [newCity]) links tunels
+foundR (Reg cities links tunels) newCity = Reg (cities ++ [newCity]) links tunels
 
 cityInRegion :: City -> Region -> Bool
 cityInRegion city (Reg [] _ _) = False
-cityInRegion city (Reg (citys : cityvs) a b) = (city == citys) || cityInRegion city (Reg cityvs a b)
+cityInRegion city (Reg (cities : cityvs) a b) = (city == cities) || cityInRegion city (Reg cityvs a b)
 
 linkR :: Region -> City -> City -> Quality -> Region -- enlaza dos ciudades de la región con un enlace de la calidad indicada
-linkR (Reg citys links tunels) city1 city2 qua
+linkR (Reg cities links tunels) city1 city2 qua
    | city1 == city2 = error "Same city"
-   | length citys <= 1 = error "Insufficient amount of citys in region for a link"
-   | not (cityInRegion city1 (Reg citys links tunels)) || not (cityInRegion city2 (Reg citys links tunels)) = error "At least one city not in region"
-   | otherwise = Reg citys (links ++ [newL city1 city2 qua]) tunels --Enlce de las ciudades 
+   | length cities <= 1 = error "Insufficient amount of cities in region for a link"
+   | not (cityInRegion city1 (Reg cities links tunels)) || not (cityInRegion city2 (Reg cities links tunels)) = error "At least one city not in region"
+   | otherwise = Reg cities (links ++ [newL city1 city2 qua]) tunels --Enlce de las ciudades 
 
-cityToConnections :: [City] -> [Link] -> [Bool]
-cityToConnections [] _ = []
-cityToConnections [x] _ = []
-cityToConnections (x:y:ys) links = (linkIn (x,y) links) : cityToConnections (y:ys) links
-
-linkIn :: (City,City) -> [Link] -> Bool
-linkIn (c1,c2) links = foldr (\each fold -> if linksL c1 c2 each then True else fold) False links
-
-isAll target = foldr (\each -> (&&) $ target == each) True
 
 linkForTunel :: [City] -> [Link] -> [Link]
 linkForTunel [] _ = []
@@ -44,21 +33,27 @@ linkForTunel [x] _ = []
 linkForTunel (x:y:ys) links = (linkIn2 (x,y) links) : linkForTunel (y:ys) links
 
 linkIn2 :: (City,City) -> [Link] -> Link
-linkIn2 (c1,c2) links = foldr (\each fold -> if linksL c1 c2 each then each else fold) l1 links
+linkIn2 (c1,c2) links = foldr (\each fold -> if linksL c1 c2 each then each else fold) noLinkFound links
+
+countA target = foldr (\each fold -> if target == each then fold + 1 else fold) 0
 
 tunelR :: Region -> [ City ] -> Region -- genera una comunicación entre dos ciudades distintas de la región
-tunelR (Reg citys links tunels) ciudades | length ciudades <= 1 = error "Insufficient amount of citys in region for a tunel"
-                                         | not (isAll True (cityToConnections ciudades links)) = error "Not all cities are connected"
-                                         | otherwise = Reg citys links (tunels ++ [newT (linkForTunel citys links)])
+tunelR (Reg cities links tunels) ciudades | length ciudades <= 1 = error "Insufficient amount of cities in region for a tunel"
+                                         | countA noLinkFound (linkForTunel cities links) /= 0 = error "Not all cities are connected"
+                                         | otherwise = Reg cities links (tunels ++ [newT (linkForTunel cities links)])
 
 connectedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan conectadas por un tunel
-connectedR = undefined
+connectedR (Reg cities links tunels) c1 c2 = foldr (\each fold -> if connectsT c1 c2 each then True else fold) False tunels
 
 linkedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan enlazadas
-linkedR = undefined
+linkedR (Reg cities links tunels) c1 c2 = foldr (\each fold -> if linksL c1 c2 each then True else fold) False links
+
+reqTunel :: Region -> City -> City -> Tunel
+reqTunel (Reg cities links tunels) c1 c2 = foldr (\each fold -> if connectsT c1 c2 each then each else fold) noTunFound tunels
 
 delayR :: Region -> City -> City -> Float -- dadas dos ciudades conectadas, indica la demora
-delayR = undefined
+delayR r1@(Reg cities links tunels) c1 c2 | connectedR r1 c1 c2 = delayT (reqTunel r1 c1 c2)
+                                       | otherwise = error "Cities not connected"
 
 availableCapacityForR :: Region -> City -> City -> Int -- indica la capacidad disponible entre dos ciudades
 availableCapacityForR = undefined
@@ -71,9 +66,12 @@ c4 = newC "chino" (newP 4 4)
 c5 = newC "nombre" (newP 5 5)
 c6 = newC "BsAs" (newP 6 6)
 
+
 l1 = newL c1 c2 qua1 
 l2 = newL c2 c3 qua1
 l3 = newL c3 c4 qua1
+noLinkFound = newL (newC "noLinkFound" (newP 0 0)) (newC "noLinkFound" (newP 0 0)) (newQ "noLinkFound" 0 0)
+noTunFound = newT [noLinkFound]
 
 qua1 = newQ "naaa" 10 10
 
@@ -86,18 +84,18 @@ region3 = Reg [c2, c3] [] []
 
 linksTrue = [newL c1 c2 qua1, newL c2 c3 qua1, newL c3 c4 qua1, newL c4 c1 qua1]
 linksFalse = [newL c1 c2 qua1, newL c2 c3 qua1, newL c3 c1 qua1]
-linE1 = cityToConnections [c1,c2,c3] linksTrue
-linE2 = cityToConnections [c1,c2,c4] linksFalse
+linE1 = linkForTunel [c1,c2,c3] linksTrue
+linE2 = linkForTunel [c1,c2,c4] linksFalse
 linkPepe = [newL c1 c3 qua1, newL c3 c2 qua1, newL c1 c2 qua1]
 linkQuavo = [newL c1 c3 qua1, newL c3 c2 qua1, newL c2 c4 qua1]
-linE3 = cityToConnections [c1, c2] linkPepe
-linE4 = cityToConnections [c1, c2] linkQuavo 
+linE3 = linkForTunel [c1, c2] linkPepe
+linE4 = linkForTunel [c1, c2] linkQuavo 
 
 
 linkeado = [newL c1 c3 qua1, newL c3 c5 qua1, newL c2 c3 qua1]
-linE5 = cityToConnections [c1, c2, c3, c4, c5, c6] linkeado -- [False, True, False, False, False, False]
+linE5 = linkForTunel [c1, c2, c3, c4, c5, c6] linkeado
 linkeado2 = [newL c1 c2 qua1, newL c3 c4 qua1, newL c5 c6 qua1]
-linE6 = cityToConnections [c1, c2, c3, c4, c5, c6] linkeado2 -- [True, False, True, False, True]
+linE6 = linkForTunel [c1, c2, c3, c4, c5, c6] linkeado2
 
 tunr1 = tunelR region1 [c1,c2,c3]
 tunr2 = tunelR region1 [c1]
