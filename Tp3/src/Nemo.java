45abdbcee7dc2605156f02670ef70168c1e1264b
jojojab <jojojablon@gmail.com>
2023-10-05 09:32:11 -0300

    cambios de Peter

--- Tp3/src/Nemo.java
+++ Tp3/src/Nemo.java
@@ -20,4 +20,45 @@ public class Nemo {
     public Integer posY() { return position.get(1); }
     public Integer altitude() { return altitude; }
     public Integer direction() { return direction; }
+
+    public void move(String moves ) {
+        for (String move : moves.split("")) {
+            switch (move) {
+                case "d":
+                    altitude--;
+                    break;
+                case "u":
+                    if (altitude < 0) {
+                        altitude++;
+                    }
+                    break;
+                case "l":
+                    direction = (direction + 90) % 360;
+                    break;
+                case "r":
+                    if ((direction - 90) < 0) {
+                        direction = 270;
+                    } else {
+                        direction = direction - 90;
+                    }
+                    break;
+                case "f":
+                    if (direction == 0) {
+                        position = Arrays.asList(posX() + 1, posY());
+                    } else if (direction == 90) {
+                        position = Arrays.asList(posX(), posY() + 1);
+                    } else if (direction == 180) {
+                        position = Arrays.asList(posX() - 1, posY());
+                    } else if (direction == 270) {
+                        position = Arrays.asList(posX(), posY() - 1);
+                    }
+                    break;
+                case "m":
+                    liberar();
+                    break;
+            }
+        }
+    }
+    public void liberar(){
+    }
 }
